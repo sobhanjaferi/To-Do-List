0b8ba9e1269c7d2bc85a3b820c3a691e4fed109e
sobhan <marktporg@gmail.com>
2025-06-04 08:47:26 +0330

    add list histore day

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 let addItem = document.getElementById("input_add");
 let button = document.getElementById("button");
 let listItems = document.getElementById("list_items");
+let listItemsHistore = document.getElementById("list_items_histore");
+let listItemsHistoreContainer = document.querySelector(".histore_container");
 
 button.addEventListener("click",()=>{
     listItems.style.display = "inline"
 
     let newItem = document.createElement("li");
+    let closeIcon = document.createElement("i");
+
     let newItemText = document.createTextNode(addItem.value);
+    let newCloseIcon = document.createTextNode("⨉")
 
-    newItem.style.borderBottom = "2px solid #0984e3";
-    newItem.style.padding = "10px";
-    newItem.style.marginRight = "15px";    
-
+    closeIcon.appendChild(newCloseIcon);
     newItem.appendChild(newItemText);
+    newItem.appendChild(closeIcon)
     listItems.appendChild(newItem);
 
     addItem.value = "";
+
+    closeIcon.addEventListener("click",()=>{
+
+        listItemsHistoreContainer.style.display = "flex"
+        closeIcon.style.display = "none"
+        newItem.style.borderBottom = "2px dashed black"
+        listItemsHistore.appendChild(newItem);
+        
+    })
 })
